8470260905e18f492cec9221be0c01a0ef83bd22
Daniel <daniel.protsak@gmail.com>
2019-11-13 00:02:51 +0200

    added some functionality to BookView

--- src/main/webapp/WEB-INF/pages/main-page.jsp
+++ src/main/webapp/WEB-INF/pages/main-page.jsp
@@ -1,11 +1,22 @@
 <%@include file="/WEB-INF/static/header.jsp"%>
 <main id="main">
 	<div class="container main-page-container">
-		<div class="row">
-			<div class="col-2">
-				<p>TEST</p>
-			</div>
+		 <div class="row first-row-in-main-page">
+			<!-- <div class="col-4">
+				<div class="book-container">
+					<img src="https://www.moscowbooks.ru/image/book/575/w259/i575271.jpg" alt="" class = "book-img-size">
+					<a href="">Book name</a>
+				</div>
+			</div>  -->
 		</div>
 	</div>
 </main>
+<script src = "scripts/BookView.js"></script>
+<script>
+	console.log(document.querySelector(".row.first-row-in-main-page"));
+	window.BookView.render({name:"HUY",imgSource:"img/logolabrary-cover.jpg"},document.querySelector(".row.first-row-in-main-page"));
+	window.BookView.render({name:"HUY",imgSource:"img/logolabrary-cover.jpg"},document.querySelector(".row.first-row-in-main-page"));
+	window.BookView.render({name:"HUY",imgSource:"img/logolabrary-cover.jpg"},document.querySelector(".row.first-row-in-main-page"));
+	 window.BookView.render({name:"HUY",imgSource:"img/logolabrary-cover.jpg"},document.querySelector(".row.first-row-in-main-page"));
+</script>
 <%@include file="/WEB-INF/static/footer.jsp"%>
--- src/main/webapp/scripts/BookView.js
+++ src/main/webapp/scripts/BookView.js
@@ -1,9 +1,37 @@
 (function(){
 
-window.BookView = {
+var BookView = {
 
-	render: function(movie,element){
-		
+	render: function(book,element){
+		if(element.children.length  < 4){
+			var book1Colum = document.createElement("div");
+			book1Colum.classList.add("col-4");
+
+			var book1Container = document.createElement("div");
+			book1Container.classList.add("book-container");
+			var bookImg = document.createElement("img");
+			bookImg.setAttribute("src",book.imgSource);
+			bookImg.classList.add("book-img-size");
+			var bookLinkingName = document.createElement("a");
+			bookLinkingName.textContent = book.name;
+			bookLinkingName.setAttribute("href","");
+			book1Container.appendChild(bookImg);
+			book1Container.appendChild(bookLinkingName);
+			book1Colum.appendChild(book1Container);
+			element.appendChild(book1Colum);
+		}else{
+			if(element.nextSibling != null){
+				this.render(book,element.nextSibling);
+			}else{
+				var mainPageContainer = document.querySelector(".container.main-page-container");
+				var row = document.createElement("div");
+				row.classList.add("row");
+				mainPageContainer.appendChild(row);
+				this.render(book,row);
+			}
+		}
 	}
 }
+
+window.BookView = BookView;
 }());
